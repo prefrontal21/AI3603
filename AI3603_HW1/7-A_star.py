9873f48c7082b742f964018f0c79c7c3f6f1d54e
sjx <sjtu_xiaosong@sjtu.edu.cn>
2021-10-08 14:02:51 +0800

    format 7-A_star.py

--- AI3603_HW1/7-A_star.py
+++ AI3603_HW1/7-A_star.py
@@ -1,31 +1,30 @@
 import DR20API
 import numpy as np
 import sys
-import math
 
 ### START CODE HERE ###
 # This code block is optional. You can define your utility function and class in this block if necessary.
+
+
 class Point:
-    def __init__(self, x,y):
+    def __init__(self, x, y, parent=None):
         self.x = x
         self.y = y
-        self.parent =None
+        self.parent = parent
         self.f = sys.maxsize
         self.h = sys.maxsize
         self.total_cost = sys.maxsize
 
-    def set_parent(self, parent):
-        self.parent = parent
-
-    def process_as_start(self,goal_pos):
+    def process_as_start(self, goal_pos):
         self.f = 0
-        self.h =abs(self.x - goal_pos[0]) + abs(self.y -goal_pos[1]) #+(math.sqrt(2)-2)*min(abs(self.x - goal_pos[0]),abs(self.y -goal_pos[1]))
+        self.h = abs(self.x - goal_pos[0]) + abs(self.y - goal_pos[1])
         self.total_cost = self.h
 
     def process_with_parent(self, goal_pos):
-        self.f = self.parent.f + abs(self.x -self.parent.x)+ abs(self.y - self.parent.y)
-        self.h = abs(self.x - goal_pos[0]) + abs(self.y -goal_pos[1]) #+(math.sqrt(2)-2)*min(abs(self.x - goal_pos[0]),abs(self.y -goal_pos[1]))
-        self.total_cost = self.f +self.h
+        self.f = self.parent.f + \
+            abs(self.x - self.parent.x) + abs(self.y - self.parent.y)
+        self.h = abs(self.x - goal_pos[0]) + abs(self.y - goal_pos[1])
+        self.total_cost = self.f + self.h
 
 
 class A_star_class:
@@ -37,7 +36,7 @@ class A_star_class:
         self.closed_set = []
 
     def process_start(self):
-        current_point = Point(self.current_pos[0],self.current_pos[1])
+        current_point = Point(self.current_pos[0], self.current_pos[1])
         current_point.process_as_start(goal_pos)
         self.open_set.append(current_point)
 
@@ -48,77 +47,75 @@ class A_star_class:
         for p in self.open_set:
             if p.total_cost <= min_cost:
                 min_cost = p.total_cost
-                select_id =id
-            id+=1
+                select_id = id
+            id += 1
         return select_id
 
     def in_set(self, p, set):
         for point in set:
-            if (p.x - point.x ==0) & (p.y - point.y ==0):
+            if (p.x - point.x == 0) and (p.y - point.y == 0):
                 return True
         return False
 
-    def in_open_set(self,p):
-        return self.in_set(p,self.open_set)
+    def in_open_set(self, p):
+        return self.in_set(p, self.open_set)
 
-    def in_closed_set(self,p):
-        return self.in_set(p,self.closed_set)
+    def in_closed_set(self, p):
+        return self.in_set(p, self.closed_set)
 
     def process(self, x, y, parent):
-        if (x<0) | (x>= 120) | (y<0) | (y>=120):
+        if (x < 0) or (x >= 120) or (y < 0) or (y >= 120):
             return
-        if self.current_map[x][y]==1:
+        if self.current_map[x][y] == 1:
             return
 
-        p = Point(int(x),int(y))
+        p = Point(int(x), int(y))
         if self.in_closed_set(p):
             return
         if not self.in_open_set(p):
-            p.set_parent(parent)
+            p.parent = parent
             p.process_with_parent(self.goal_pos)
             self.open_set.append(p)
 
-    def reached(self,p):
-        if (p.x == self.goal_pos[0]) & (p.y ==self.goal_pos[1]):
+    def reached(self, p):
+        if (p.x == self.goal_pos[0]) & (p.y == self.goal_pos[1]):
             return True
         return False
 
-    def build_path(self,p):
+    def build_path(self, p):
         path = []
-        path.append([int(p.x),int(p.y)])
+        path.append([int(p.x), int(p.y)])
         while True:
             p = p.parent
             if p:
-                path.insert(0,[int(p.x),int(p.y)])
+                path.insert(0, [int(p.x), int(p.y)])
             if (p.x == self.current_pos[0]) & (p.y == self.current_pos[1]):
                 return path
         return path
 
     def run(self):
         self.process_start()
-        print("current pos:", self.current_pos[0],self.current_pos[1])
+        print("current pos:", self.current_pos[0], self.current_pos[1])
         while True:
-            id =self.select_min_cost()
+            id = self.select_min_cost()
             p = self.open_set[id]
             self.closed_set.append(p)
             del self.open_set[id]
             if(self.reached(p)):
                 path = self.build_path(p)
-                path.append([100,101])
-                #print(path)
+                path.append([100, 101])
+                # print(path)
                 return path
 
-            #print(p.x,p.y,self.current_map[p.x][p.y],p.f,p.h,p.total_cost)
-            self.process(p.x-1,p.y-1,p)
-            self.process(p.x-1, p.y,p)
-            self.process(p.x-1, p.y+1,p)
-            self.process(p.x, p.y-1,p)
-            self.process(p.x, p.y+1,p)
-            self.process(p.x+1, p.y-1,p)
-            self.process(p.x+1, p.y,p)
-            self.process(p.x+1, p.y+1,p)
+            # print(p.x,p.y,self.current_map[p.x][p.y],p.f,p.h,p.total_cost)
+            self.process(p.x-1, p.y, p)
+            self.process(p.x, p.y-1, p)
+            self.process(p.x, p.y+1, p)
+            self.process(p.x+1, p.y, p)
 
 ###  END CODE HERE  ###
+
+
 def A_star(current_map, current_pos, goal_pos):
     """
     Given current map of the world, current position of the robot and the position of the goal, 
@@ -134,13 +131,15 @@ def A_star(current_map, current_pos, goal_pos):
     """
 
     ### START CODE HERE ###
-    current_pos = [int(current_pos[0]),int(current_pos[1])]
-    goal_pos =[int(goal_pos[0]),int(goal_pos[1])]
-    algorithm = A_star_class(current_map,current_pos,goal_pos)
+    current_pos = [int(current_pos[0]), int(current_pos[1])]
+    goal_pos = [int(goal_pos[0]), int(goal_pos[1])]
+
+    algorithm = A_star_class(current_map, current_pos, goal_pos)
     path = algorithm.run()
 
     ###  END CODE HERE  ###
     return path
+
 
 def reach_goal(current_pos, goal_pos):
     """
@@ -156,12 +155,13 @@ def reach_goal(current_pos, goal_pos):
     """
 
     ### START CODE HERE ###
-    if (current_pos[0] - goal_pos[0] == 0) & (current_pos[1] - goal_pos[1] ==0):
-        is_reached =True
+    if (current_pos[0] - goal_pos[0] == 0) & (current_pos[1] - goal_pos[1] == 0):
+        is_reached = True
     else:
         is_reached = False
     ###  END CODE HERE  ###
     return is_reached
+
 
 if __name__ == '__main__':
     # Define goal position of the exploration, shown as the gray block in the scene.
